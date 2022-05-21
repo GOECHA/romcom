4d59a192c124ee402abe44c06ecf7fe8c3ec33c6
David Stavis <davidstavis@gmail.com>
2022-05-20 18:26:53 -0600

    Refactor randomize to utilize an instance of the Cover class

--- src/main.js
+++ src/main.js
@@ -4,38 +4,29 @@ var coverTitle = document.querySelector('.cover-title')
 var coverTagLine = document.querySelector('.tagline')
 var priceTag = document.querySelector('.price-tag')
 var randomBtn = document.querySelector('.random-cover-button')
-var makeNewBtn = document.querySelector('.make-new-button')
-var makeOwnPage = document.querySelector('form')
-
 
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
-var currentCover;
 
 // Add your event listeners here 👇
 randomBtn.addEventListener('click', randomize)
-makeNewBtn.addEventListener('click', showMakeOwnPage)
 
 // Create your event handlers and other functions here 👇
 randomize();
 function randomize() {
-    var newCover = getRandomIndex(covers)
-    var newTitle = getRandomIndex(titles)
-    var newTag1 = getRandomIndex(descriptors)
-    var newTag2 = getRandomIndex(descriptors)
-    coverPic.src = covers[newCover]
-    coverTitle.innerText = titles[newTitle]
-    coverTagLine.innerText = `A tale of ${descriptors[newTag1]} and ${descriptors[newTag2]}`
-    return new Cover(covers[newCover], titles[newTitle], descriptors[newTag1], descriptors[newTag2])
+    var newCoverIndex = getRandomIndex(covers)
+    var newTitleIndex = getRandomIndex(titles)
+    var newTag1Index = getRandomIndex(descriptors)
+    var newTag2Index = getRandomIndex(descriptors)
+
+    var newCover = new Cover(covers[newCoverIndex], titles[newTitleIndex], descriptors[newTag1Index], descriptors[newTag2Index])
+
+    coverPic.src = newCover.cover
+    coverTitle.innerText = newCover.title
+    coverTagLine.innerText = `A tale of ${newCover.tagline1} and ${newCover.tagline2}`
 }
-
-function showMakeOwnPage() {
-  console.log(makeOwnPage)
-}
-
-
 
 
 // We've provided one function to get you started
